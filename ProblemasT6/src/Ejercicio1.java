32e12bb2112ff4781028fc65de64e4216551c16e
Alex <alexrosca180596@gmail.com>
2017-01-24 21:02:47 +0100

    Problema 1

---
+++ ProblemasT6/src/Ejercicio1.java
@@ -0,0 +1,51 @@
+import java.util.*;
+public class Ejercicio1 {
+
+
+	public static void main(String[] args) {
+		Scanner teclado=new Scanner(System.in);
+		Random rnd=new Random();
+
+		int[] x=new int[20];
+
+		for(int i=0;i<20;i++)
+			x[i]=-10+rnd.nextInt(21);
+
+		imprimirArray(x);
+
+		int suma=0,media=0,superior=0,inferior=0,igual=0;
+
+		//calcular la suma
+		for(int i=0;i<x.length;i++){
+			suma+=x[i];
+		}
+		//calcular la media 
+		media=suma/x.length;
+		//superiores a la media
+		for(int i=0;i<x.length;i++){
+			if(x[i]>media)
+				superior++;
+			else 
+				if(x[i]==media)
+					igual++;
+				else
+					inferior++;
+
+		}
+		System.out.println("Media: "+media+" Superiores: "+superior+" Inferiores: "+inferior+" Igual: "+igual);
+
+	}
+
+	static void rellenarArray(int[] x){
+
+
+	}
+
+	static void imprimirArray(int[] x){
+		for(int i=0;i<x.length;i++)
+			System.out.print(x[i] +" | ");
+
+		System.out.println();
+	}
+
+}
